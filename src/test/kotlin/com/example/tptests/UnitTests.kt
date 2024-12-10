25da55be55a2f5a8b26b55464c78b4d8e78df999
vbetsch <vic.betsch@gmail.com>
2024-12-10 12:38:57 +0100

    :art: Group tests by context

--- src/test/kotlin/com/example/tptests/UnitTests.kt
+++ src/test/kotlin/com/example/tptests/UnitTests.kt
@@ -9,102 +9,51 @@ import io.kotest.property.arbitrary.int
 import io.kotest.property.checkAll
 
 class UnitTests : FunSpec({
-    // Examples
-    test("put letter 'A' and number '2' should returns letter 'C'") {
-        // Arrange
-        val letter = 'A'
-        val number = 2
-        val cesar = CesarCypher()
-
-        // Act
-        val res = cesar.cypher(letter = letter, shift = number)
-
-        // Assert
-        res shouldBe 'C'
-    }
-    test("put letter 'A' and number '5' should returns letter 'F'") {
-        // Arrange
-        val letter = 'A'
-        val number = 5
-        val cesar = CesarCypher()
-
-        // Act
-        val res = cesar.cypher(letter = letter, shift = number)
-
-        // Assert
-        res shouldBe 'F'
-    }
-    test("put letter 'X' and number '4' should returns letter 'B'") {
-        // Arrange
-        val letter = 'X'
-        val number = 4
-        val cesar = CesarCypher()
-
-        // Act
-        val res = cesar.cypher(letter = letter, shift = number)
-
-        // Assert
-        res shouldBe 'B'
-    }
-    test("put letter 'a' should returns an error") {
-        // Arrange
-        val letter = 'a'
-        val number = 7
-        val cesar = CesarCypher()
-
-        // Act
-        fun act() = cesar.cypher(letter = letter, shift = number)
-
-        // Assert
-        val exception = shouldThrow<IllegalArgumentException> {
-            act()
-        }
-        exception.message shouldBe "Letter must be uppercase"
-    }
-    test("put number '-1' should returns an error") {
-        // Arrange
-        val letter = 'A'
-        val number = -1
-        val cesar = CesarCypher()
-
-        // Act
-        fun act() = cesar.cypher(letter = letter, shift = number)
-
-        // Assert
-        val exception = shouldThrow<IllegalArgumentException> {
-            act()
-        }
-        exception.message shouldBe "Number must be positive"
-    }
-
-    // Properties
-    test("put any letter and number '0' should returns input letter") {
-        val alphabet = ('A'..'Z').toMutableList()
-
-        val upperCaseLetters = arbitrary { alphabet.random() }
-
-        checkAll(upperCaseLetters) { anyLetter ->
+    context("Examples") {
+        test("put letter 'A' and number '2' should returns letter 'C'") {
             // Arrange
+            val letter = 'A'
+            val number = 2
             val cesar = CesarCypher()
 
             // Act
-            val res = cesar.cypher(letter = anyLetter, shift = 0)
+            val res = cesar.cypher(letter = letter, shift = number)
 
             // Assert
-            res shouldBe anyLetter
+            res shouldBe 'C'
         }
-    }
-    test("put any lowercase letter should returns an error") {
-        val alphabet = ('a'..'z').toMutableList()
-
-        val lowerCaseLetters = arbitrary { alphabet.random() }
-
-        checkAll(lowerCaseLetters) { anyLetter ->
+        test("put letter 'A' and number '5' should returns letter 'F'") {
             // Arrange
+            val letter = 'A'
+            val number = 5
             val cesar = CesarCypher()
 
             // Act
-            fun act() = cesar.cypher(letter = anyLetter, shift = 0)
+            val res = cesar.cypher(letter = letter, shift = number)
+
+            // Assert
+            res shouldBe 'F'
+        }
+        test("put letter 'X' and number '4' should returns letter 'B'") {
+            // Arrange
+            val letter = 'X'
+            val number = 4
+            val cesar = CesarCypher()
+
+            // Act
+            val res = cesar.cypher(letter = letter, shift = number)
+
+            // Assert
+            res shouldBe 'B'
+        }
+        test("put letter 'a' should returns an error") {
+            // Arrange
+            val letter = 'a'
+            val number = 7
+            val cesar = CesarCypher()
+
+            // Act
+            fun act() = cesar.cypher(letter = letter, shift = number)
 
             // Assert
             val exception = shouldThrow<IllegalArgumentException> {
@@ -112,17 +61,70 @@ class UnitTests : FunSpec({
             }
             exception.message shouldBe "Letter must be uppercase"
         }
-    }
-    test("put any negative number should returns an error") {
-        val negativeNumbers = Arb.int(Int.MIN_VALUE, -1)
-        val cesar = CesarCypher()
+        test("put number '-1' should returns an error") {
+            // Arrange
+            val letter = 'A'
+            val number = -1
+            val cesar = CesarCypher()
 
-        checkAll(negativeNumbers) { anyNumber ->
-            fun act() = cesar.cypher(letter = 'A', shift = anyNumber)
+            // Act
+            fun act() = cesar.cypher(letter = letter, shift = number)
+
+            // Assert
             val exception = shouldThrow<IllegalArgumentException> {
                 act()
             }
             exception.message shouldBe "Number must be positive"
+        }
+    }
+
+    context("Properties") {
+        test("put any letter and number '0' should returns input letter") {
+            val alphabet = ('A'..'Z').toMutableList()
+
+            val upperCaseLetters = arbitrary { alphabet.random() }
+
+            checkAll(upperCaseLetters) { anyLetter ->
+                // Arrange
+                val cesar = CesarCypher()
+
+                // Act
+                val res = cesar.cypher(letter = anyLetter, shift = 0)
+
+                // Assert
+                res shouldBe anyLetter
+            }
+        }
+        test("put any lowercase letter should returns an error") {
+            val alphabet = ('a'..'z').toMutableList()
+
+            val lowerCaseLetters = arbitrary { alphabet.random() }
+
+            checkAll(lowerCaseLetters) { anyLetter ->
+                // Arrange
+                val cesar = CesarCypher()
+
+                // Act
+                fun act() = cesar.cypher(letter = anyLetter, shift = 0)
+
+                // Assert
+                val exception = shouldThrow<IllegalArgumentException> {
+                    act()
+                }
+                exception.message shouldBe "Letter must be uppercase"
+            }
+        }
+        test("put any negative number should returns an error") {
+            val negativeNumbers = Arb.int(Int.MIN_VALUE, -1)
+            val cesar = CesarCypher()
+
+            checkAll(negativeNumbers) { anyNumber ->
+                fun act() = cesar.cypher(letter = 'A', shift = anyNumber)
+                val exception = shouldThrow<IllegalArgumentException> {
+                    act()
+                }
+                exception.message shouldBe "Number must be positive"
+            }
         }
     }
 })
