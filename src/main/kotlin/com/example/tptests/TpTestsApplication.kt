e3ae98e567e57d43c44c1666c6cd8d89df008d6a
vbetsch <vic.betsch@gmail.com>
2024-12-10 09:39:31 +0100

    :tada: Initial commit

---
+++ src/main/kotlin/com/example/tptests/TpTestsApplication.kt
@@ -0,0 +1,11 @@
+package com.example.tptests
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class TpTestsApplication
+
+fun main(args: Array<String>) {
+    runApplication<TpTestsApplication>(*args)
+}
