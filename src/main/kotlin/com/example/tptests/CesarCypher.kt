85b3e8b834d6a5bb0afcf7d7e48207cf4f74e681
vbetsch <vic.betsch@gmail.com>
2024-12-10 10:57:55 +0100

    :sparkles: Test case

--- src/main/kotlin/com/example/tptests/CesarCypher.kt
+++ src/main/kotlin/com/example/tptests/CesarCypher.kt
@@ -4,6 +4,12 @@ class CesarCypher {
     private val alphabet = ('A'..'Z').toMutableList()
 
     fun cypher(letter: Char, shift: Int): Char {
+        if (letter.isLowerCase()) {
+            throw IllegalArgumentException(
+                "Letter must be uppercase",
+                IllegalStateException("Original cause: illegal state")
+            )
+        }
         return alphabet.elementAt(alphabet.indexOf(letter) + shift)
     }
 }
--- src/test/kotlin/com/example/tptests/AppTest.kt
+++ src/test/kotlin/com/example/tptests/AppTest.kt
@@ -1,10 +1,11 @@
 package com.example.tptests
 
+import io.kotest.assertions.throwables.shouldThrow
 import io.kotest.core.spec.style.FunSpec
 import io.kotest.matchers.shouldBe
 
 class AppTest : FunSpec({
-    test("put letter A and number 2 should returns letter C") {
+    test("put letter 'A' and number '2' should returns letter 'C'") {
         // Arrange
         val letter = 'A'
         val number = 2
@@ -16,16 +17,19 @@ class AppTest : FunSpec({
         // Assert
         res shouldBe 'C'
     }
-    test("put letter C and number 7 should returns letter J") {
+    test("put letter 'a' should return an error") {
         // Arrange
-        val letter = 'C'
+        val letter = 'a'
         val number = 7
         val cesar = CesarCypher()
 
         // Act
-        val res = cesar.cypher(letter = letter, shift = number)
+        fun act() = cesar.cypher(letter = letter, shift = number)
+
 
         // Assert
-        res shouldBe 'J'
+        shouldThrow<IllegalArgumentException> {
+            act()
+        }
     }
 })
